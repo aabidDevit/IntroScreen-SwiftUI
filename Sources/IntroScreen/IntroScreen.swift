c9432ae6be1dab8f84114af8a0b592f51a651258
aabid khan <aabid.khan@devitpl.com>
2024-06-10 18:43:01 +0530

    Title and Description made dynamic

--- Sources/IntroScreen/IntroScreen.swift
+++ Sources/IntroScreen/IntroScreen.swift
@@ -18,9 +18,13 @@ public struct Intro: View {
     var nextButtonLabel: Text
     var skipButtonLabel: Text
     var getStartedButtonLabel: Text
+    var titleLabel: Text
+    var descriptionLabel: Text
     var hidePageNumberLabel: Bool
     
-    public init(selectedTab: Int = 0, getStartedLabel: Text, introDataArray: [IntroData], prevButtonLabel: Text, nextButtonLabel: Text, skipButtonLabel: Text, hidePageNumberLabel: Bool = false) {
+    public init(titleLabel: Text, descriptionLabel: Text, selectedTab: Int = 0, getStartedLabel: Text, introDataArray: [IntroData], prevButtonLabel: Text, nextButtonLabel: Text, skipButtonLabel: Text, hidePageNumberLabel: Bool = false) {
+        self.titleLabel = titleLabel
+        self.descriptionLabel = descriptionLabel
         self.selectedTab = selectedTab
         self.getStartedButtonLabel = getStartedLabel
         self.introDataArray = introDataArray
@@ -49,7 +53,7 @@ public struct Intro: View {
                 Button(action: {
                     
                 }, label: {
-                   skipButtonLabel
+                    skipButtonLabel
                 })
                 .frame(maxWidth: .infinity, alignment: .trailing)
                 .padding(.trailing)
@@ -60,7 +64,7 @@ public struct Intro: View {
                 
                 TabView(selection: $selectedTab) {
                     ForEach(Array(introDataArray.enumerated()), id: \.element.id) { index, item in
-                        IntroContentView(data: item, imageInyourAsset: Image(item.imageAssetName), titleText: Text(item.titleText), descText: Text(item.desc))
+                        IntroContentView(data: item, imageInyourAsset: Image(item.imageAssetName), titleText: titleLabel, descText: descriptionLabel)
                             .tag(index)
                     }
                 }
@@ -74,7 +78,7 @@ public struct Intro: View {
                         Button(action: {
                             selectedTab -= 1
                         }, label: {
-                           prevButtonLabel
+                            prevButtonLabel
                         })
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.leading)
