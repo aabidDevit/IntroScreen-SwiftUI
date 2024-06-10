75e5c8180e5ef8296cec7fa5b3e3ac149bbbe090
aabid khan <aabid.khan@devitpl.com>
2024-06-10 18:46:01 +0530

    Image key changed

--- Sources/IntroScreen/IntroContentView.swift
+++ Sources/IntroScreen/IntroContentView.swift
@@ -10,29 +10,24 @@ import SwiftUI
 struct IntroContentView: View {
     
     var data: IntroData
-    let imageInyourAsset: Image
+    let image: Image
     let titleText: Text
     let descText: Text
     
     var body: some View {
         
         VStack {
-            imageInyourAsset
-//            Image(data.image)
+            image
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: K.Size.widthInProportionOfScreen(width: 350), height: K.Size.heightInProportionOfScreen(height: 350))
                 .padding(.bottom, 15)
                 
-//            Text(data.title)
-//                .font(.custom(K.Fonts.themeExtraBold, size: 24))
             titleText
                 .padding(.bottom, 10)
-//            Text(data.desc)
+
             descText
                 .frame(width: K.Size.widthInProportionOfScreen(width: 340))
-//                .foregroundStyle(Color(.borderColorGray))
-//                .font(.custom(K.Fonts.themeSemiBold, size: 14))
                 .multilineTextAlignment(.center)
             
             Spacer()
@@ -43,5 +38,5 @@ struct IntroContentView: View {
 }
 
 #Preview {
-    IntroContentView(data:IntroData(title: "Test", desc: "Desc", image: ""), imageInyourAsset: Image(""), titleText: Text(""), descText: Text(""))
+    IntroContentView(data:IntroData(title: "Test", desc: "Desc", image: ""), image: Image(""), titleText: Text(""), descText: Text(""))
 }
